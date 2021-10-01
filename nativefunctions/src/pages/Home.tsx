5a42a800ea3448c6ec3d50edffa1c87f185d57f4
Binh Do <binhdq@fe.edu.vn>
2021-10-01 10:36:35 +0700

    display picture

--- nativefunctions/src/pages/Home.tsx
+++ nativefunctions/src/pages/Home.tsx
@@ -1,11 +1,22 @@
 import { IonButton, IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
 import './Home.css';
 
 const Home: React.FC = () => {
   var myPlayer: ReactAudioPlayer | null
-
+  const [pictureURL, setPictureURL] = useState('assets/placeHolder.jpeg')
+  
+  function selectFileHandle(event: React.ChangeEvent<HTMLInputElement>){
+    if(event.target.files != null){
+      const fileName = event.target.files[0].name
+      //create an URL for the file so it can be displayed by image component
+      const picURL = URL.createObjectURL(event.target.files[0])
+      console.log(pictureURL);
+      setPictureURL(picURL);
+    }
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +27,7 @@ const Home: React.FC = () => {
       <IonContent className="ion-padding">
         <IonList>
           <IonItem>
-            <ReactAudioPlayer 
+            <ReactAudioPlayer
               src="assets\music.mp3"
               ref={(element) => { myPlayer = element }}
             />
@@ -24,7 +35,14 @@ const Home: React.FC = () => {
             <IonButton onClick={() => myPlayer?.audioEl.current?.pause()}>Pause</IonButton>
           </IonItem>
           <IonItem>
-            <IonButton onClick={()=>navigator.vibrate(2500)}>Vibration</IonButton>
+            <IonButton onClick={() => navigator.vibrate(2500)}>Vibration</IonButton>
+          </IonItem>
+          <IonItem>
+            <input type="file" onChange={selectFileHandle}></input>
+          </IonItem>
+          <IonItem>
+            <img src={pictureURL} />
+            <IonButton>Upload</IonButton>
           </IonItem>
         </IonList>
       </IonContent>
