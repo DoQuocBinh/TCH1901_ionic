89c6b45eb2dc201b73ae3d109ea4d1b805d73501
DoQuocBinh <binhdq@fpt.edu.vn>
2021-10-08 11:27:01 +0700

    ok

--- cameraDemo/src/pages/Home.tsx
+++ cameraDemo/src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useState } from 'react';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useEffect, useState } from 'react';
 import {
   Camera,
   CameraResultType,
@@ -8,23 +8,40 @@ import {
 } from "@capacitor/camera";
 
 import './Home.css';
-import { insertPicture } from '../databaseHandler';
+import { getAllPics, insertPicture } from '../databaseHandler';
+
+interface PicInfo {
+  id: number,
+  note: string,
+  picBlob: Blob
+}
 
 const Home: React.FC = () => {
   const [pictureURL, setPictureURL] = useState('assets/placeholder.jpg');
-  const [pictureNote,setPictureNote] = useState('')
+  const [pictureNote, setPictureNote] = useState('')
+  const [pics, setPics] = useState<PicInfo[]>([])
+  const [refreshFlag,setRefreshFlag] = useState(false)
+
+  async function fetchData() {
+    const result = await getAllPics()
+    setPics(result)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [refreshFlag])
 
   async function handleSave() {
     //download picture from URL
     const response = await fetch(pictureURL)
     const fileContent = await response.blob()
     //construct object to insert
-    const obj = {note: pictureNote, picBlob:fileContent}
+    const obj = { note: pictureNote, picBlob: fileContent }
     await insertPicture(obj)
-    alert('Insert completed!')
+    setRefreshFlag(!refreshFlag)
   }
 
-  async function  takePicture(){
+  async function takePicture() {
     const cameraPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Prompt,
@@ -41,16 +58,25 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <IonItem lines="none">
-          <img src={pictureURL} width="150" height="120"/>
+          <img src={pictureURL} width="150" height="120" />
         </IonItem>
         <IonItem>
           <IonLabel color="warning" position="floating">Picture notes</IonLabel>
-          <IonInput onIonChange={e=>setPictureNote(e.detail.value!)}></IonInput>
+          <IonInput onIonChange={e => setPictureNote(e.detail.value!)}></IonInput>
         </IonItem>
         <IonItem>
           <IonButton onClick={takePicture}>Select Picture</IonButton>
           <IonButton onClick={handleSave}>Save</IonButton>
         </IonItem>
+
+        <IonList>
+          {pics.map(p =>
+            <IonItem key={p.id} button>
+              <IonLabel>{p.note}</IonLabel>
+              <img src={URL.createObjectURL(p.picBlob)} width="80" height="60" />
+            </IonItem>
+          )}
+        </IonList>
       </IonContent>
     </IonPage>
   );
